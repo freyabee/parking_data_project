6e4e467d83d981d76b3140b27e30e393b89db6db
Brightwater <freya.brightwater@siemens.com>
2019-01-29 09:15:30 +0000

    Map is displayed

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask import render_template
+import os
+
+
 
 app = Flask(__name__)
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- static/main.js
+++ static/main.js
@@ -1,1 +1,10 @@
 // custom javascript
+
+BASE_COORDINATES = [50.7192, -1.8808]
+
+var map = L.map('map').setView(BASE_COORDINATES, 11);
+var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+});
+map.addLayer(layer);
+console.log("Read!!!")
