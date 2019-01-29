c5c1347bdfb53380f4d021db9a0be3c6bbe7b1ed
Brightwater <freya.brightwater@siemens.com>
2019-01-29 09:38:40 +0000

    Test heat dot displayed

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
--- static/main.js
+++ static/main.js
@@ -7,4 +7,9 @@ var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 });
 map.addLayer(layer);
+
+var heat = L.heatLayer([
+	[50.7192, -1.8808, 50], // lat, lng, intensity
+], {radius: 40}).addTo(map);
+
 console.log("Read!!!")
