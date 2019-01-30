087d8674eb703082663439a43881150c5304b7af
Brightwater <freya.brightwater@siemens.com>
2019-01-30 11:22:58 +0000

    1 geojson

--- app.py
+++ app.py
@@ -27,8 +27,16 @@ def initmap():
 
     parking = folium.Map(location=[50.7260218,-1.8827525], zoom_start = 13, tiles="CartoDB dark_matter")
     hm = plugins.HeatMapWithTime(list_data, index=[date for date in unique], auto_play=False,radius=40, max_opacity=0.8)
+    hm.layer_name = 'Heatmap'
     parking.add_child(hm)
 
+
+    #Load GeoJson
+    json14 =  os.path.join('data', 'carparkmap.json')
+    folium.GeoJson(
+        json14,
+        name='geojson').add_to(parking)
+    folium.LayerControl().add_to(parking)
     parking.save(outfile='templates/map.html')
 
 
