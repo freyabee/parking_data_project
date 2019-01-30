7b327d6cfefbc50a1ef29680b2e0a37e4b889a0e
Brightwater <freya.brightwater@siemens.com>
2019-01-30 14:02:33 +0000

    added minimap

--- app.py
+++ app.py
@@ -25,14 +25,23 @@ def initmap():
         templist = [[row['latitude'],row['longitude'],row['norm_occupancy']] for index, row in tempdf.iterrows()]
         list_data.append(templist)
 
-    parking = folium.Map(location=[50.7260218,-1.8827525], zoom_start = 13, tiles="CartoDB dark_matter")
+    parking = folium.Map(location=[50.7260218,-1.8827525], zoom_start = 13, tiles="OpenStreetMap")
     hm = plugins.HeatMapWithTime(list_data, index=[date for date in unique], auto_play=False,radius=40, max_opacity=0.8)
     hm.layer_name = 'Heatmap'
     parking.add_child(hm)
 
+    minimap = plugins.MiniMap()
+    parking.add_child(minimap)
+    
+    json14 =  os.path.join('data', 'carparkmap.json')    
+    for property_name, property_value in features["json14"].items():
+        
+        print(property_name, property_value)
+
+
 
     #Load GeoJson
-    json14 =  os.path.join('data', 'carparkmap.json')
+
     folium.GeoJson(
         json14,
         name='geojson').add_to(parking)
