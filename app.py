fcf4a9e5f9106ed6233b7cc0b8fc1f70970bc55b
TheAltbacker <william.altbacker@gmail.com>
2019-01-31 15:08:40 +0000

    api call prototype for the original asset

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 import os
+import requests
+from inspect import currentframe, getframeinfo
+import json
 
 import pandas as pd
 import numpy as np 
@@ -18,6 +21,32 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("map.html")
+	
+@app.route('/')
+def asset_data():
+    url = 'https://gateway.eu1.mindsphere.io/api/assetmanagement/v3/assets/{0}/aspects/'.format("2780aabba30d4b1083c284adb80b1f09")
+    try:
+        response = requests.get(url + "2780aabba30d4b1083c284adb80b1f09", headers=requestHeaders())
+        if not response.status_code == 200:
+            eprintMS(getframeinfo(currentframe()), response.content)
+            return render_template('index.html', assets_data = None, error = "MindSphere API Error / Failed to get assets")
+        data = json.loads(response.text)
+        return render_template('index.html', assets_data = data, error = None)
+    except requests.ConnectionError:
+        eprintMS(getframeinfo(currentframe()), "MindSphere failed to connect to itself")
+        return render_template('index.html', assets_data = data, error = "MindSphere failed to connect to itself")
+
+# Prints detailed error messages
+def eprintMS(frameinfo, message):
+    print(frameinfo.filename, ":", frameinfo.lineno, "/", message)
+
+def requestHeaders():
+    try:
+        int(os.getenv("VCAP_APP_PORT"))!= 5000 #app is running in cloud foundry
+        headers =  {'Authorization': request.headers["Authorization"]}
+    except:
+        headers = {'Authorization': 'Bearer ' + token['access_token']}
+    return headers
 
 if __name__ == "__main__":
     appEnv = "Cloud Foundry"
